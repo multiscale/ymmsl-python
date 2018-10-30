e788387746f754c7899f25492332e470692aa15f
Lourens Veen <l.veen@esciencecenter.nl>
2018-10-30 15:04:56 +0100

    Add more Ymmsl loading tests

--- tests/test_ymmsl.py
+++ tests/test_ymmsl.py
@@ -44,20 +44,6 @@ def test_loader(caplog: Any) -> None:
             '    test_str: value\n'
             '    test_int: 13\n'
             '    test_list: [12.3, 1.3]\n'
-            'simulation:\n'
-            '  name: test_model\n'
-            '  compute_elements:\n'
-            '    ic: isr2d.initial_conditions\n'
-            '    smc: isr2d.smc\n'
-            '    bf: isr2d.blood_flow\n'
-            '    smc2bf: isr2d.smc2bf\n'
-            '    bf2smc: isr2d.bf2smc\n'
-            '  conduits:\n'
-            '    ic.out: smc.initial_state\n'
-            '    smc.cell_positions: smc2bf.in\n'
-            '    smc2bf.out: bf.initial_domain\n'
-            '    bf.wss_out: bf2smc.in\n'
-            '    bf2smc.out: smc.wss_in\n'
             )
     document = yaml.load(text, Loader=ymmsl.loader)
     experiment = document.experiment
@@ -71,6 +57,36 @@ def test_loader(caplog: Any) -> None:
     assert experiment.scales[1].extent == 100.0
     assert len(experiment.parameter_values) == 3
     assert experiment.parameter_values[2].value[1] == 1.3
+
+    text = 'version: v0.1\n'
+    document = yaml.load(text, Loader=ymmsl.loader)
+    assert document.version == 'v0.1'
+
+    text = ('version: v0.1\n'
+            'simulation:\n'
+            '  name: test_model\n'
+            '  compute_elements:\n'
+            '    ic: isr2d.initial_conditions\n'
+            '    smc: isr2d.smc\n'
+            '    bf: isr2d.blood_flow\n'
+            '    smc2bf: isr2d.smc2bf\n'
+            '    bf2smc: isr2d.bf2smc\n'
+            '  conduits:\n'
+            '    ic.out: smc.initial_state\n'
+            '    smc.cell_positions: smc2bf.in\n'
+            '    smc2bf.out: bf.initial_domain\n'
+            '    bf.wss_out: bf2smc.in\n'
+            '    bf2smc.out: smc.wss_in\n')
+    document = yaml.load(text, Loader=ymmsl.loader)
+    simulation = document.simulation
+    assert str(simulation.name) == 'test_model'
+    assert len(simulation.compute_elements) == 5
+    assert str(simulation.compute_elements[4].name) == 'bf2smc'
+    assert str(simulation.compute_elements[2].implementation) == 'isr2d.blood_flow'
+    assert len(simulation.conduits) == 5
+    assert str(simulation.conduits[0].sender) == 'ic.out'
+    assert str(simulation.conduits[1].sending_port()) == 'cell_positions'
+    assert str(simulation.conduits[3].receiving_compute_element()) == 'bf2smc'
 
 
 def test_dumper() -> None:
