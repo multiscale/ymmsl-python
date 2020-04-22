5f7bff3fe85fc12f477641cd087692309cb910ad
Lourens Veen <l.veen@esciencecenter.nl>
2020-04-22 19:18:48 +0200

    Update metadata

--- setup.py
+++ setup.py
@@ -24,14 +24,15 @@ setup(
     zip_safe=False,
     keywords='yMMSL multiscale modeling simulation YAML',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     test_suite='tests',
     install_requires=[
