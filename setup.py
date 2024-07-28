b39db0ca7a452e9805cee872293a30ad93802fca
Lourens Veen <l.veen@esciencecenter.nl>
2024-07-28 21:40:25 +0200

    Add Python 3.12 and remove 3.7

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     python_requires='>=3.7, <4',
     test_suite='tests',
