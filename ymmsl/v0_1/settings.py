multiscale/ymmsl-python

commit 67e4aafdf4ab6d970a89e332c8b9e17958944c43

Fix contains operator for Settings

ymmsl/v0_1/settings.py
@@ -2,7 +2,7 @@
 from collections import OrderedDict
 from collections.abc import MutableMapping
 from copy import deepcopy
-from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
+from typing import Any, Dict, Iterator, List, Optional, overload, Tuple, TypeVar, Union
 
 import yatiml
 
@@ -12,6 +12,9 @@ from ymmsl.v0_1.identity import Reference
 SettingValue = Union[
         str, int, float, bool, List[int], List[float], List[List[float]],
         yatiml.bool_union_fix]
+
+
+_T = TypeVar('_T')
 
 
 class Settings(MutableMapping):
@@ -53,6 +56,14 @@ class Settings(MutableMapping):
         """Represent as a string."""
         return str(self.as_ordered_dict())
 
+    def __contains__(self, key: object) -> bool:
+        """Returns whether an item with the given name is set."""
+        if isinstance(key, str):
+            key = Reference(key)
+        if not isinstance(key, Reference):
+            return False
+        return key in self._store
+
     def __getitem__(self, key: Union[str, Reference]) -> SettingValue:
         """Returns an item, implements settings[name]."""
         if isinstance(key, str):
@@ -79,6 +90,25 @@ class Settings(MutableMapping):
     def __len__(self) -> int:
         """Returns the number of settings."""
         return len(self._store)
+
+    @overload
+    def get(self, key: Any, /) -> Any | None: ...
+
+    @overload
+    def get(
+            self, key: Any, /, default: _T) -> Union[Any, _T]: ...
+
+    def get(
+            self, key: Any, /, default: Union[_T, None] = None) -> Union[Any, _T]:
+        """Return the given setting, or default if it is not set.
+
+        If default is not given, returns None.
+        """
+        if isinstance(key, str):
+            key = Reference(key)
+        if not isinstance(key, Reference):
+            return default
+        return self._store.get(key, default)
 
     def ordered_items(self) -> List[Tuple[Reference, SettingValue]]:
         """Return settings as a list of tuples."""

ymmsl/v0_1/tests/test_settings.py
@@ -89,6 +89,15 @@ def test_to_string(settings: Settings) -> None:
     assert '42' in str(settings)
 
 
+def test_contains(settings: Settings) -> None:
+    assert 'test' not in settings
+    assert Reference('test') not in settings
+
+    settings['test'] = 1
+    assert 'test' in settings
+    assert Reference('test') in settings
+
+
 def test_get_item(settings: Settings) -> None:
     settings._store[Reference('test')] = 13
     assert settings[Reference('test')] == 13

commit 85f16fdc1a39a9365673b8c7da02ee6c90fffa6d

Fix Python 3.9 broken in 67e4aaf

ymmsl/v0_1/settings.py
@@ -92,7 +92,7 @@ class Settings(MutableMapping):
         return len(self._store)
 
     @overload
-    def get(self, key: Any, /) -> Any | None: ...
+    def get(self, key: Any, /) -> Union[Any, None]: ...
 
     @overload
     def get(