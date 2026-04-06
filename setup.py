fe74aad976ee8e357ca39f2a3f568c34ae46ae36
Lourens Veen <l.veen@esciencecenter.nl>
2026-04-06 11:47:36 +0200

    Set release version to 0.15.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='ymmsl',
-    version='0.14.1-dev',
+    version='0.15.0',
     description="Python bindings for the YAML version of the Multiscale Modeling and Simulation Language",
     long_description=readme + '\n\n',
     author="Lourens Veen",
