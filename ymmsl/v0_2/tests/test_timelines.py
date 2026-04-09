74a4a87e63da37772c4c56e9755b574e38565efa
Maarten Sebregts <msebregts@ignitioncomputing.com>
2026-04-09 16:37:43 +0200

    Update timelines logic
    - Make most TimelineTree variables "private". Accessor functions to be created for public API.
    - Add tests

---
+++ ymmsl/v0_2/tests/test_timelines.py
@@ -0,0 +1,104 @@
+from ymmsl.v0_2.timelines import TimelineTree
+from pathlib import Path
+import ymmsl
+from ymmsl.v0_2 import Configuration, Reference as Ref, Timeline, ConduitFilter
+
+import pytest
+
+ROOT_TIMELINE = Timeline(":")
+
+@pytest.fixture()
+def timelines_configuration() -> Configuration:
+    return ymmsl.load_as(Configuration, Path(__file__).parent / "ymmsl1/timelines.ymmsl")
+
+
+def test_consistent_configuration(timelines_configuration: Configuration) -> None:
+    timelines_configuration.check_consistent()
+
+
+def test_dispatch(timelines_configuration: Configuration) -> None:
+    tltree = TimelineTree(timelines_configuration.models[Ref("dispatch")])
+    assert tltree.component_timeline(Ref("first")) == ROOT_TIMELINE
+    assert tltree.component_timeline(Ref("second")) == ROOT_TIMELINE
+
+
+def test_macromicro(timelines_configuration: Configuration) -> None:
+    tltree = TimelineTree(timelines_configuration.models[Ref("macromicro")])
+    assert tltree.component_timeline(Ref("macro")) == ROOT_TIMELINE
+    assert tltree.component_timeline(Ref("micro")) == Timeline(":macro")
+
+
+def test_cycle(timelines_configuration: Configuration) -> None:
+    with pytest.raises(RuntimeError, match="Cycle detected"):
+        TimelineTree(timelines_configuration.models[Ref("cycle")])
+
+
+def test_reducer(timelines_configuration: Configuration) -> None:
+    tltree = TimelineTree(timelines_configuration.models[Ref("reducer")])
+    assert tltree.component_timeline(Ref("first")) == ROOT_TIMELINE
+    assert tltree.component_timeline(Ref("second")) == ROOT_TIMELINE
+
+
+def test_too_many_reducers(timelines_configuration: Configuration) -> None:
+    model = timelines_configuration.models[Ref("reducer")]
+    model.conduits[-1].filters.append(model.conduits[-1].filters[0])
+    with pytest.raises(ValueError, match="Too many reducer filters"):
+        TimelineTree(model)
+
+
+def test_inconsistent_timelines(timelines_configuration: Configuration) -> None:
+    with pytest.raises(ValueError, match="Inconsistent timelines"):
+        TimelineTree(timelines_configuration.models[Ref("inconsistent")])
+
+
+def test_repeaters(timelines_configuration: Configuration) -> None:
+    tltree = TimelineTree(timelines_configuration.models[Ref("repeaters")])
+    assert tltree.component_timeline(Ref("macro")) == ROOT_TIMELINE
+    assert tltree.component_timeline(Ref("meso")) == Timeline(":macro")
+    assert tltree.component_timeline(Ref("micro")) == Timeline(":macro:meso")
+
+
+def test_too_many_repeaters(timelines_configuration: Configuration) -> None:
+    model = timelines_configuration.models[Ref("repeaters")]
+    model.conduits[-2].filters.append(ConduitFilter.REPEAT)
+    with pytest.raises(ValueError, match="Too many repeater filters"):
+        TimelineTree(model)
+
+
+def test_too_few_repeaters(timelines_configuration: Configuration) -> None:
+    model = timelines_configuration.models[Ref("repeaters")]
+    model.conduits[-1].filters.pop()
+    with pytest.raises(ValueError, match="Inconsistent timelines"):
+        TimelineTree(model)
+
+
+def test_repeater_and_too_many_reducers(timelines_configuration: Configuration) -> None:
+    model = timelines_configuration.models[Ref("repeaters")]
+    model.conduits[-1].filters.insert(0, ConduitFilter.LAST)
+    with pytest.raises(ValueError, match="Too many reducer filters"):
+        TimelineTree(model)
+
+
+def test_repeater_after_reducer(timelines_configuration: Configuration) -> None:
+    model = timelines_configuration.models[Ref("repeater_reducer")]
+    tltree = TimelineTree(model)
+    assert tltree.component_timeline(Ref("macro1")) == ROOT_TIMELINE
+    assert tltree.component_timeline(Ref("macro2")) == ROOT_TIMELINE
+    assert tltree.component_timeline(Ref("micro1")) == Timeline(":macro1")
+    assert tltree.component_timeline(Ref("micro2")) == Timeline(":macro2")
+
+    # Removing the filters on the last conduit is a problem
+    model.conduits[-1].filters = []
+    with pytest.raises(ValueError, match="Inconsistent timelines"):
+        TimelineTree(model)
+
+
+def test_repeater_after_reducer_error(timelines_configuration: Configuration) -> None:
+    model = timelines_configuration.models[Ref("repeater_reducer_error")]
+    with pytest.raises(ValueError, match="repeater after reducer"):
+        TimelineTree(model)
+    model.conduits[-1].filters = []
+    tltree = TimelineTree(model)
+    assert tltree.component_timeline(Ref("macro")) == ROOT_TIMELINE
+    assert tltree.component_timeline(Ref("micro1")) == Timeline(":macro")
+    assert tltree.component_timeline(Ref("micro2")) == Timeline(":macro")
--- ymmsl/v0_2/timelines.py
+++ ymmsl/v0_2/timelines.py
@@ -14,35 +14,29 @@ class TimelineTree:
     """Determines timelines and nesting for a given yMMSL model"""
 
     def __init__(self, model: Model) -> None:
-        self.model = model
+        self._model = model
         """yMMSL model that this TimelineTree belongs to."""
         self.root = TimelineNode(Timeline(":"), None)
         """Timeline node of the root timeline."""
 
-        self.component_timeline: dict[Reference, TimelineNode] = {}
+        self._component_timeline: dict[Reference, TimelineNode] = {
+            Reference([]): self.root,  # Support model ports
+        }
         """Map of component names to the TimelineNode they are part of."""
-        self.conduits_by_receiver: dict[Reference, Conduit] = {
-            conduit.receiver: conduit for conduit in self.model.conduits
+        self._conduits_by_receiver: dict[Reference, Conduit] = {
+            conduit.receiver: conduit for conduit in self._model.conduits
         }
         """Map of conduits by their receiving port"""
-        self.all_ports: dict[Reference, Port] = {
+        self._all_ports: dict[Reference, Port] = {
             component.name + port_name: port
-            for component in self.model.components.values()
+            for component in self._model.components.values()
             for port_name, port in component.ports.items()
         }
         """Map of Port objects by their full reference"""
 
-        # Ports may only have relative timeline annotations of size 1
-        for port in self.all_ports.values():
-            if port.timeline.absolute or len(port.timeline) > 1:
-                # FIXME?
-                raise ValueError(
-                    f"Invalid timeline annotation for {port.name}: {port.timeline}"
-                )
-
         # Assign components to timelines
-        for component in self.model.components.values():
-            if component.name in self.component_timeline:
+        for component in self._model.components.values():
+            if component.name in self._component_timeline:
                 continue
             self._assign_component(component, [])
 
@@ -51,7 +45,7 @@ class TimelineTree:
         result = []
         for port in component.ports.values():
             if port.operator is Operator.F_INIT:
-                conduit = self.conduits_by_receiver.get(component.name + port.name)
+                conduit = self._conduits_by_receiver.get(component.name + port.name)
                 if conduit is not None:
                     result.append(conduit)
         return result
@@ -63,15 +57,15 @@ class TimelineTree:
             idx = seen.index(component)
             cycle_list = seen[idx:] + [component]
             cycle = " -> ".join(str(component.name) for component in cycle_list)
-            raise RuntimeError(f"Cycle detected in model '{self.model.name}': {cycle}")
+            raise RuntimeError(f"Cycle detected in model '{self._model.name}': {cycle}")
         f_init_conduits = self._f_init_conduits_for_component(component)
 
         # Ensure we know the timelines of the components attached to our F_INIT
         seen.append(component)
         for conduit in f_init_conduits:
             sender = conduit.sending_component()
-            if sender not in self.component_timeline:
-                self._assign_component(self.model.components[sender], seen)
+            if sender not in self._component_timeline:
+                self._assign_component(self._model.components[sender], seen)
         seen.pop()
 
         # Now we can determine our timeline
@@ -81,10 +75,11 @@ class TimelineTree:
             if any(filter.is_repeater() for filter in conduit.filters):
                 repeater_conduits.append(conduit)
                 continue
-            timeline = self.timeline_for_sending_port(conduit.sender)
+            timeline = self._timeline_for_sending_port(conduit.sender)
             for filter in conduit.filters:
                 assert filter.is_reducer()
                 if timeline.parent is None:
+                    # TODO: better error message
                     raise ValueError("Too many reducer filters")
                 timeline = timeline.parent
             incoming_timelines.append(timeline)
@@ -98,41 +93,47 @@ class TimelineTree:
 
         # Check consistency of conduits with repeaters
         for conduit in repeater_conduits:
-            timeline1 = self.timeline_for_sending_port(conduit.sender)
+            timeline1 = self._timeline_for_sending_port(conduit.sender)
             timeline2 = determined_timeline
             for filter in conduit.filters:
                 if timeline1 is determined_timeline:
+                    # TODO: better error message
                     raise ValueError(
                         "repeater after reducer cannot be in same timeline"
                     )
                 if filter.is_reducer():
                     if timeline1.parent is None:
+                        # TODO: better error message
                         raise ValueError("Too many reducer filters")
                     timeline1 = timeline1.parent
                 if filter.is_repeater():
                     if timeline2.parent is None:
+                        # TODO: better error message
                         raise ValueError("Too many repeater filters")
                     timeline2 = timeline2.parent
             if timeline1 is not timeline2:
+                # TODO: better error message
                 raise ValueError("Inconsistent timelines")
 
         # Done: register timeline for component
-        self.component_timeline[component.name] = determined_timeline
+        self._component_timeline[component.name] = determined_timeline
         determined_timeline.add(component)
 
-    def timeline_for_sending_port(self, port_name: Reference) -> "TimelineNode":
+    def _timeline_for_sending_port(self, port_name: Reference) -> "TimelineNode":
+        """Determine the resulting timeline for a component that connects (with f_init)
+        to the provided port name."""
         component = port_name[:-1]
         if len(component) == 0:
             # Connected to a model port
             model_port = port_name[-1]
             assert isinstance(model_port, Identifier)
-            port = self.model.ports[model_port]
+            port = self._model.ports[model_port]
             if port.operator is Operator.F_INIT:
                 return self.root
             else:
                 raise NotImplementedError()  # FIXME?
-        timeline = self.component_timeline[component]
-        port = self.all_ports[port_name]
+        timeline = self._component_timeline[component]
+        port = self._all_ports[port_name]
         if port.operator is Operator.O_F:
             return timeline
         assert port.operator is Operator.O_I
@@ -141,6 +142,10 @@ class TimelineTree:
             # No explicit label attached to the timeline, so we take the component name
             subtimeline = Timeline(str(component))
         return timeline.get(subtimeline)
+
+    def component_timeline(self, component: Reference) -> Timeline:
+        """Get the determined timeline for a component in the model"""
+        return self._component_timeline[component].name
 
 
 class TimelineNode:
@@ -153,28 +158,33 @@ class TimelineNode:
     def __init__(self, timeline: Timeline, parent: "TimelineNode | None") -> None:
         """Initialize timeline node for a given timeline"""
 
-        self.name = timeline
+        self.name: Timeline = timeline
         """Name of this timeline."""
-        self.parent = parent
+        self._parent: TimelineNode | None = parent
         """Parent node of this node."""
-        self.children: dict[Reference, TimelineNode] = {}
+        self._children: dict[Reference, TimelineNode] = {}
         """Child timeline nodes."""
-        self.components: list[Component] = []
+        self._components: list[Component] = []
         """List of components that are part of this timeline."""
+
+    @property
+    def parent(self) -> "TimelineNode | None":
+        """Parent timeline node, or None if this is the root timeline."""
+        return self._parent
 
     def get(self, subtimeline: Timeline) -> "TimelineNode":
         """Get a sub-timeline of this timeline, creating a new one if required."""
         assert not subtimeline.absolute
         assert len(subtimeline) == 1
-        child = self.children.get(subtimeline[0])
+        child = self._children.get(subtimeline[0])
         if child is None:
             child = TimelineNode(self.name + subtimeline, self)
-            self.children[subtimeline[0]] = child
+            self._children[subtimeline[0]] = child
         return child
 
     def add(self, component: Component) -> None:
         """Add component to this timeline."""
-        self.components.append(component)
+        self._components.append(component)
 
     def __repr__(self) -> str:
         return f"TimelineNode({self.name})"
