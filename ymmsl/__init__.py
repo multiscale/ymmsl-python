b7432636dbc89d5cf093b8c729f4fe0c9d999cdd
Lourens Veen <l.veen@esciencecenter.nl>
2026-06-03 21:47:39 +0200

    Import __version__ from package metadata to avoid mismatches

--- ymmsl/__init__.py
+++ ymmsl/__init__.py
@@ -3,6 +3,8 @@
 This package contains all the classes needed to represent a yMMSL file,
 as well as to read and write yMMSL files.
 """
+import importlib
+
 from ymmsl.conversion.converter import convert_to, DowngradeError
 from ymmsl.document import Document
 from ymmsl.io import dump, load, load_as, save
@@ -11,7 +13,7 @@ from ymmsl.io import dump, load, load_as, save
 from ymmsl.v0_2 import Operator, Settings
 
 
-__version__ = '0.15.1-dev'
+__version__ = importlib.metadata.version("ymmsl")
 __author__ = 'Lourens Veen'
 __email__ = 'l.veen@esciencecenter.nl'
 
